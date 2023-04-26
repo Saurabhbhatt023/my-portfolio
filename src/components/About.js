8dfa272bf15e435e12df8e83140427d0f2909b32
Saurabh bhatt <85731069+Saurabhbhatt023@users.noreply.github.com>
2023-04-26 21:13:08 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -10,13 +10,17 @@ const About = () => {
          </div>
       </div>
       <p className='text-xl mt-20'> 
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        I  have  a exprience in  Front-End-Devlopment
+       Technology i which i have worked 
+       ⬇️
       </p>
       <br/> 
       <p> 
-
-      Lorem ipsum dolor sit amet, consectetur 
-      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+         JavaScript
+         Reactjs
+         TypeScript
+         Tailwind
+         HTML , Css
 
          
       </p>
