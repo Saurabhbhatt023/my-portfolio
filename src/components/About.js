7acaf2bae278edcae76d5f2c61a3056ba990d6db
Saurabh bhatt <85731069+Saurabhbhatt023@users.noreply.github.com>
2023-04-27 07:43:10 +0530

    Update About.js
    adding color

--- src/components/About.js
+++ src/components/About.js
@@ -9,21 +9,27 @@ const About = () => {
          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
          </div>
       </div>
-      <p className='text-xl mt-20'> 
-        I  have  a exprience in  Front-End-Devlopment
-       Technology i which i have worked 
+      <p className='text-xl mt-20 pl-20'> 
+        I  have a  Good exprience in  Front-End-Devlopment
+       Technology i have worked with 
        ⬇️
       </p>
       <br/> 
       <p> 
-        <ul> 
-      <li>
-         JavaScript
-         Reactjs
-         TypeScript
-         Tailwind
-         HTML , Css 
-     </li>
+        <ul className='pl-20'> 
+
+        <li className='hover:bg-orange-500 hover:text-black'> HTML </li>
+        <li className='hover:bg-sky-400 hover:text-black'> Css </li>
+        <li className='hover:bg-yellow-400 hover:text-black'> Javascript </li>
+        <li className='hover:bg-blue-500 hover:text-black'> Reactjs</li>
+        <li className='hover:bg-violet-400 hover:text-black'> Redux</li>
+        <li className='hover:bg-blue-700 hover:text-black'> TypeScript</li>
+        <li className='hover:bg-sky-300 hover:text-black'> Tailwind</li>
+         
+        
+       
+  
+    
         </ul>
          
       </p>
